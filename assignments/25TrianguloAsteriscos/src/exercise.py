ea1246aa39ddf78b31404e86ecd7bd4dd80ab877
AlexisLok <a01251886@itesm.mx>
2021-09-06 01:56:20 +0000

    Ejercicio 2 de Tarea 4

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,6 +1,16 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    N = int(input("Enter triangle height: "))
+    espacio = " "
+    star = "*"
+    conEspacio = N - 1
+    conStar = N - conEspacio 
+    
+    for x in range(0,N):
+        print(espacio * conEspacio + star*conStar)  
+        conEspacio -= 1
+        conStar += 1
 
 
 if __name__=='__main__':
